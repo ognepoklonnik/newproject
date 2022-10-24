22f90acebc352b76d131c83965602c7ed8af5d56
ognepoklonnik <tside.computers@gmail.com>
2022-10-24 21:00:30 +0300

    logic description

--- src/Body/index.js
+++ src/Body/index.js
@@ -32,30 +32,48 @@ function Body(
   const [x2, setX2] = useState(0);
 
   useEffect(() => {
+    //внутри открыли асинхронную функцию которая моментально и запускается.
+    // немеделенно вызываемая функция 
     (async function () {
+      //try - пробует выполнить всё что в его scope
+      // при обнаружении ошибки try останавливается и запускает catch scope {} передав ему ошибку
       try {
+        // мы спрашиваем если ли ответ с API в состоянии компонента
         if(!response) {
+          // если нету то мы делаем запрос и сохраняем в состоянии компонента
           const response = await getPriceData();
           setResponse(response.data);
-          return;
+          return; // гарантируем чтобы код дальше не выполнялся
         }
+        // взяв ответ с api мы выбираем выбранную пользователем страну 
+        // на массив с данными мы запускаем цыкл MAP который  возвращает нам новый массив
         let priceData = response[selectedCountry.key].map(dataObject => {
+          // в цыкле мы с timestamp взяли часы "НН" и назначили x и у
+          // у = цена
+          // х = время 
+          // timestamp = unix timestamp = сколько секунд прошло с 01.01.1970
           return {
             x: moment.unix(dataObject.timestamp).format("HH"),
             y: dataObject.price,
             timestamp: dataObject.timestamp,
           };
         });
-        setData(priceData);
-        
+        setData(priceData); //назначили новый массив с обработанными данными
 
+        //ищем index в котором записан данный часс
         const hourNowI = priceData.findIndex((dataObject) => {
           return dataObject.x === moment().format("HH");
         });
         setHourNowI(hourNowI);
 
+        // выделяем/фильтруем массив где только будующее время,поскольку знаем чир будущие времяя наступает после 9 индекса
         const futureData = priceData.filter((v, i) => i >= 9);
         const areaPrices = [];
+
+        // допустим ищем 3 самых дешёвых часа,запускаем цыкл на будующие временна
+        // каждый цыкл берёт 3 часа / 3 элемента с массива
+        // суммирует их и записывает в новый массив с текущим indexom
+        //таким образом мы находим самый дешовый диапазон в 3 часа
         futureData.forEach((v, i, arr) => {
           const partData = arr.slice(i, i + hourValue + 1);
           if (partData.length === hourValue + 1) {
@@ -65,15 +83,21 @@ function Body(
           }
           return;
         });
+
+        // сортируем по сумме дешовые в начало
         areaPrices.sort((a, b) => a.result - b.result);
         if (radioValue === 'low'){
+          // если ходим знать самые дешовые цены
+          // берём будующие времена и ищем обьект с ценой по первому / дешёвому индексу
+          // состовляем наши данные для графика и счётчика
           setBestTimeRange({
           from: futureData[areaPrices[0].i + hourValue].x,
           until: futureData[areaPrices[0].i + hourValue].x,
           timestamp: futureData[areaPrices[0].i].timestamp,
           bestPrice: futureData[areaPrices[0].i].y,
           });
-        } else {
+          // если хотим самые дорогие то мы переварачиваем самые дешёвые суммы
+        } else { 
           areaPrices.reverse();
           setWorstTimeRange({
             from: futureData[areaPrices[0].i + hourValue].x,
@@ -81,7 +105,7 @@ function Body(
             worstPrice: futureData[areaPrices[0].i].y,
             });
         }
-        
+        // добавляем прошлое для графика и назначаем точки выбранного 
           setX1(9 + areaPrices[0].i);
           const x2 = 9 + areaPrices[0].i + hourValue;
           setX2(x2);
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,6 +1,13 @@
-import moment from 'moment';
+import moment from 'moment';  // npm пакет для работы с датой/число и время
 
-const apiUrl = 'https://dashboard.elering.ee/api';
+const apiUrl = 'https://dashboard.elering.ee/api';  // api url
+
+
+//экспортируем асинхронную функцию которая запрашивает текущую стоимость электорэнергии.
+// запрос GET /nps/price/ee/current - endpoint
+//await позволяет даждаться ответа с api
+// fetch возвращает Promise/обещание и при правильном выполнении обьект Response.
+//каждый response имеет функцию .json() которая переводит JSON в js обьект.
 
 export async function getCurrentPrice () {
     const country = 'EE';
@@ -9,8 +16,15 @@ export async function getCurrentPrice () {
 };
 
 export async function getPriceData() {
+
+    //moment() - выдает moment object в текущем времени и датой 
+    // .uts() - конвертирует его в нулевой часовой пояс
+    // substract - вычетает
+    // .format() - превращает moment object в Строчку String с удобным форматом чтения
+
     const start = moment().utc().subtract(10, 'hours').format();
     const end = moment().utc().add(30, 'hours').format();
+    // URLSearchParams  - превращает js object в строчку для url
     const params = new URLSearchParams({start, end});
     const response = await fetch(`${apiUrl}/nps/price?${params}`);
     return response.json();
